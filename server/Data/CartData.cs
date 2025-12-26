ec2417729b7087f25154efedec44fffab9a79cbc
Shime <s95187463@gmail.com>
2025-12-26 12:51:47 +0800

    到ProductController.cs新增API成功，抓取全部資料

--- server/Data/CartData.cs
+++ server/Data/CartData.cs
@@ -2,12 +2,12 @@
 
 namespace server.Data
 {
-    public class CartData
-    {
-        public static List<CartDto> datas = new()
+  public class CartData
+  {
+    public static List<CartDto> datas = new()
         {
             new CartDto { id = 1, name = "Fitbit Sense 2 智慧手錶", price = 1351,  quantity = 2, image = "/images/products/1.png" },
             new CartDto { id = 2, name = "Apple iPhone 13 Pro（天峰藍）", price = 4590, quantity = 1,  image = "/images/products/2.png" },
         };
-    }
+  }
 }
