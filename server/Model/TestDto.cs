62e17ac9e50d99db8c5fbaa922b9cc284b1ba799
Shime <s95187463@gmail.com>
2025-12-28 19:00:36 +0800

    複習完課程錄影檔全部

--- server/Model/TestDto.cs
+++ server/Model/TestDto.cs
@@ -1,10 +1,10 @@
 ﻿namespace server.Model
 {
-    public class TestDataDto
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string category { get; set; }
-        public int price { get; set; }
-    }
+  public class TestDataDto
+  {
+    public int id { get; set; }
+    public string name { get; set; }
+    public string category { get; set; }
+    public int price { get; set; }
+  }
 }
